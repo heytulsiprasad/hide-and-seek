d91ffcaac7dcd7876c1ba3652e22de0b1ebd99df
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-04-24 13:36:11 +0530

    :sparkles: add logic for the game

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ $(".select_ul li").click(function () {
     let currentEle = $(this).html();
     $(".default_option li").html(currentEle);
     $(this).parents(".nav__select_wrap").removeClass("active");
+
+    let level = $(this).children().text().trim();
+
+    if (level === "Easy") {
+
+    } else if (level === "Medium") {
+
+    } else if (level === "Hard") {
+        
+    }
 });
 
 let code = ["🎉", "💪", "😍", "🏠", "🐶", "☕", "🤷‍♂️", "😛"];
@@ -63,8 +73,86 @@ let lenEmoji = code.length;
 
 encodeItems(randLenArray, pairs, lenEmoji);
 
+// this gets the clicked emoji and compares it to the previously clicked one
+// if matched returns true, else false
+// its length is never more than 2
+let match = [];
+
+function isMatch(val) {
+    if (match.length < 1) {
+        match.push(val);
+        return "First";
+    } else if (match.length === 1) {
+        if (match.indexOf(val) === -1) {
+            match = [];
+            return false;
+        } else {
+            match = [];
+            return true;
+        }
+    }
+}
+
+// this stores the current element whose `hidden` class is removed
+let curBoxEle = [];
+
+// this stores the data-key value of the clicked element
+let curKeyVal = [];
+
 $(".item").click(function () {
-    $(this).children().toggleClass("hidden");
-    // const ele = $(this).children()[0].textContent;
-    // console.log(ele);
+    const x = $(this).children().removeClass("hidden");
+    curBoxEle.push(x);
+
+    const ele = $(this)[0];
+    const key = ele.dataset.key;
+    const emoji = ele.textContent;
+    let match;
+
+    // this makes sure if same block is clicked continously
+    // it'll show and hide the emoji respectively
+    if (curKeyVal.length < 1) {
+        curKeyVal.push(key);
+        match = isMatch(emoji);
+    } else if (curKeyVal.length === 1) {
+        if (curKeyVal.indexOf(key) === -1) {
+            curKeyVal = [];
+            curKeyVal.push(key);
+            match = isMatch(emoji);
+        } else {
+            $(this).children().addClass("hidden");
+            curKeyVal = [];
+            match = "First";
+        }
+    }
+
+    if (match === "First") {
+        console.log("Waiting...");
+    } else if (match === false) {
+        setTimeout(function () {
+            curBoxEle.forEach((item) => {
+                item.addClass("hidden");
+            });
+            curBoxEle = [];
+            console.log("Try again, harder!");
+        }, 200);
+    } else if (match === true) {
+        curBoxEle.forEach((item) => {
+            item.parent(".item").off("click");
+        });
+        curBoxEle = [];
+        console.log("Its a match!");
+        reloadIfDone();
+    }
 });
+
+function reloadIfDone() {
+    let hiddenArr = $(".hidden");
+
+    if (hiddenArr.length === 0) {
+        setTimeout(function () {
+            location.reload();
+        }, 1000);
+    } else {
+        console.log("Keep hustling!");
+    }
+}
